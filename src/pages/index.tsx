a6ae337f7375b04c25c32dbd60a44fa2d1a79a24
wisentr <mehmetefeekiner@gmail.com>
2024-06-01 18:53:42 +0200

    feat: add StaticImage plugin

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,6 +1,6 @@
 import * as React from "react";
-import { Link } from "gatsby";
 import type { HeadFC, PageProps } from "gatsby";
+import { StaticImage } from "gatsby-plugin-image";
 import Layout from "../components/layout";
 
 const IndexPage = () => {
@@ -8,6 +8,7 @@ const IndexPage = () => {
     <main>
       <Layout pageTitle="Home Page">
         <p>I'm making this by following the Gatsby Tutorial.</p>
+        <StaticImage alt="Cottage in impressionist style" src="../images/cottage.png" />
       </Layout>
     </main>
   );
