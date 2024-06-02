da7d928dc639d9f46447ceca39e7c8edc007dc38
wisentr <mehmetefeekiner@gmail.com>
2024-06-03 00:18:08 +0200

    feat: use mdx plugin, render post metadata

--- gatsby-config.ts
+++ gatsby-config.ts
@@ -24,6 +24,7 @@ const config: GatsbyConfig = {
         path: `${__dirname}/blog`,
       },
     },
+    "gatsby-plugin-mdx",
   ],
 };
 
--- src/pages/blog.tsx
+++ src/pages/blog.tsx
@@ -4,14 +4,19 @@ import Layout from '../components/layout'
 import Seo from '../components/seo'
 
 const BlogPage = ({ data }) => {
+    console.log({ data })
     return (
         <Layout pageTitle="My Blog Posts">
             <ul>
                 {
-                    data.allFile.nodes.map(node => (
-                        <li key={node.name}>
-                            {node.name}
-                        </li>
+                    data.allMdx.nodes.map(node => (
+                        <article key={node.id}>
+                            <h2>
+                                {node.frontmatter.title}
+                            </h2>
+                            <p>Posted: {node.frontmatter.date}</p>
+                            <p>{node.excerpt}</p>
+                        </article>
 
                     ))}
             </ul>
@@ -22,13 +27,17 @@ const BlogPage = ({ data }) => {
 export const Head = () => <Seo title="My Blog Posts" />
 
 export const query = graphql`
-    query{
-    allFile {
+  query {
+    allMdx(sort: { frontmatter: { date: DESC }}) {
       nodes {
-        name
+        frontmatter {
+          date(formatString: "MMMM D, YYYY")
+          title
+        }
+        id
+        excerpt
       }
     }
-  }
-  `
+  }`
 
 export default BlogPage
