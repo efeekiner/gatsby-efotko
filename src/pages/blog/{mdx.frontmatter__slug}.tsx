5b0df4f50163d6a937a53377b216648eeeb4a466
wisentr <mehmetefeekiner@gmail.com>
2026-02-01 22:12:10 +0100

    feat: enhance blog post component with dynamic data rendering and improved structure

--- src/pages/blog/{mdx.frontmatter__slug}.tsx
+++ src/pages/blog/{mdx.frontmatter__slug}.tsx
@@ -1,16 +1,33 @@
 import React from "react";
 import Layout from "../../components/layout";
 import { SEO } from "../../components/seo";
+import { graphql } from "gatsby";
 
-const BlogPost = () => {
+interface BlogPostProps {
+    data: Queries.BlogPostQuery;
+}
+
+const BlogPost = ({ data, children }: React.PropsWithChildren<BlogPostProps>) => {
+    console.log(data);
     return (
-        <Layout pageTitle="Nice Posts">
-            <p>This is where my blog posts will go!</p>
+        <Layout pageTitle={data.mdx?.frontmatter?.title ?? "Blog Post"}>
+            <p>{data.mdx?.frontmatter?.date}</p>
+            <article>{children}</article>
         </Layout>
     )
 }
 
+export const query = graphql`
+  query BlogPost($id: String) {
+    mdx(id: {eq: $id}) {
+      frontmatter {
+        title
+        date(formatString: "MMMM D, YYYY")
+      }
+    }
+  }`
 
-export const Head = () => <SEO title="Blog Post" />;
+
+export const Head = ({ data }: BlogPostProps) => <SEO title={data.mdx?.frontmatter?.title ?? "Blog Post"} />;
 
 export default BlogPost;
