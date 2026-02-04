eb7ee725ec6d33484babbb2f867d413e74fb4834
wisentr <mehmetefeekiner@gmail.com>
2026-02-04 16:31:14 +0100

    feat: make gatsbyImageData nullable in ImageSharp type to prevent build failures

--- gatsby-node.ts
+++ gatsby-node.ts
@@ -3,9 +3,19 @@ import type { GatsbyNode } from "gatsby"
 export const createSchemaCustomization: GatsbyNode["createSchemaCustomization"] = ({ actions }) => {
   const { createTypes } = actions
   
+  // Override the ImageSharp type to make gatsbyImageData nullable
+  // This prevents build failures when images are missing or fail to process
   createTypes(`
-    type ImageSharp implements Node {
+    type ImageSharp implements Node @dontInfer {
+      fixed: JSON
+      fluid: JSON
       gatsbyImageData: JSON
+      original: JSON
+      resize: JSON
+      id: ID!
+      parent: Node
+      children: [Node!]!
+      internal: Internal!
     }
   `)
 }
